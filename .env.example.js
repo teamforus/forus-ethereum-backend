fb33f34aac1e9d68f2730fd4728bcf262478ca01
Timon Back <T.Back@student.rug.nl>
2017-10-22 17:08:30 +0200

    Make ipc_path as env variable

--- .env.example.js
+++ .env.example.js
@@ -3,5 +3,7 @@ module.exports = {
     // allow request only with this key in header
     "api_key": "++4#?LZQQnP7sE!X!Vuq?$mM4F?KybmUTbg9$EghQWX_g6^+r$QcyM8zcV9$C=5F",
     // string or array of ip addresses which are allowed to use this api
-    "allowed_ip": "*"
+    "allowed_ip": "*",
+    // path to the ipc file
+    "ipc_path": "/opt/ethereum/data/geth.ipc"
 };
