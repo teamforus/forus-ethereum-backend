f5fdedc82fd2aadff3e2abb10996529ee838aa04
Timon Back <T.Back@student.rug.nl>
2017-10-22 17:58:44 +0200

    Adjusted example env file

--- .env.example.js
+++ .env.example.js
@@ -1,5 +1,9 @@
 // please generate new api_key for production and replace allowed ip 
 module.exports = {
+    // the listening host
+    "host": "0.0.0.0",
+    // the listening port
+    "port": 8500,
     // allow request only with this key in header
     "api_key": "++4#?LZQQnP7sE!X!Vuq?$mM4F?KybmUTbg9$EghQWX_g6^+r$QcyM8zcV9$C=5F",
     // string or array of ip addresses which are allowed to use this api
